c6ff39113b4aa31dfb15a2e487aebca8bc0f3a11
Rachel <racheljay42@gmail.com>
2021-01-05 14:32:08 -0500

    basic numbers showing in display

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
@@ -8,6 +8,38 @@ function App() {
   const operators = ['/', '*', '-', '+', '='];
 
   const [currentDisplay, setCurrentDisplay] = useState(0)
+  const [displayArr, setDisplayArr] = useState([0]);
+  const [clickCount, setClickCount] = useState(0)
+
+  useEffect(() => {
+    console.log('did mount')
+  })
+
+
+  const numBtnAction = (num) => {
+
+    setClickCount(clickCount + 1);
+    let arr = displayArr
+    
+    if (arr[0] === 0) {
+      arr.shift();
+      // return this.numBtnAction;
+  }
+    console.log('click count', clickCount)
+    // number = arr.join(' ')
+    console.log('num', num)
+    arr.push(num)
+    setDisplayArr(arr);
+    setCurrentDisplay(arr.join(' '))
+    console.log('arr', arr)
+    console.log('display arr', displayArr)
+    // return number;
+    // if (arr.length > 0) setCurrentDisplay(number)
+
+    // setDisplayArr(arr)
+    // console.log('state',displayArr)
+    // setCurrentDisplay(num)
+  }
 
   return (
     <div className="container">
@@ -19,12 +51,15 @@ function App() {
       <div className="row">
         <div className="col-9 border">
           <div className="row">
+
             {topRow.map((item, index) => {
-              return <div key={index} className="col-4 border">{item}</div>
+              return <div key={index} className="col-4 border" >{item}</div>
             })}
+
             {numbers.map((item, index) => {
-              return <div key={index} className="col-4 border">{item}</div>
+              return <div key={index} className="col-4 border" onClick={() => numBtnAction(item)}>{item}</div>
             })}
+
           </div>
         </div>
 
