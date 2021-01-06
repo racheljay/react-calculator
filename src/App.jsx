e2820863180adcfcc2f697d9b9af8814c12c26c2
Rachel <racheljay42@gmail.com>
2021-01-05 19:43:00 -0500

    basic addition

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,23 @@ function App() {
   const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
   const operators = ['/', '*', '-', '+', '='];
 
-  const [currentDisplay, setCurrentDisplay] = useState(0);
+  const [currentDisplay, setCurrentDisplay] = useState("0");
   const [displayArr, setDisplayArr] = useState([0]);
-  const [previousNumber, setPreviousNumber] = useState("");
+  const [previousNumber, setPreviousNumber] = useState("0");
   const [screenRefresh, setScreenRefresh] = useState(false)
 
-  useEffect(() => {
-    console.log('did mount')
-  })
+  // useEffect(() => {
+  //   console.log('did mount')
+  // })
 
 
   const numBtnAction = (num) => {
 
-    if (screenRefresh === true) {
+    if (screenRefresh) {
       setCurrentDisplay(num);
       setDisplayArr([num])
       setScreenRefresh(false);
-    } else if (screenRefresh === false) {
+    } else if (!screenRefresh) {
 
 
       let arr = displayArr
@@ -34,38 +34,60 @@ function App() {
       console.log('num', num)
       arr.push(num)
       setDisplayArr(arr);
-      setCurrentDisplay(arr.join(' '))
+      setCurrentDisplay(arr.join(''))
 
       console.log('current display type', typeof currentDisplay)
     }
-    // return number;
-    // if (arr.length > 0) setCurrentDisplay(number)
-
-    // setDisplayArr(arr)
-    // console.log('state',displayArr)
-    // setCurrentDisplay(num)
+   
   }
 
   const operatorAction = (btn) => {
     console.log(btn)
     setPreviousNumber(currentDisplay)
     setScreenRefresh(true);
+    
+    let sum = (parseInt(previousNumber) + parseInt(currentDisplay))
+    let minus = (parseInt(previousNumber) + (parseInt(currentDisplay) * -1)   )
+
+    // console.log(memory, previousNumber, currentDisplay, current)
+
+    if(btn === operators[2]) {
+      setPreviousNumber(minus.toString())
+    }
+
+    //addition
+    if(btn === operators[3]) {
+      console.log('sum', sum);
+      setPreviousNumber(sum.toString());
+    } 
+    
+    if(btn === operators[4]) {
+      setCurrentDisplay(sum.toString());
+      setPreviousNumber("0")
+    }
+    //equals
+    
+    
 
   }
 
   const topRowAction = (btn) => {
     console.log(btn)
 
+    //clear out
     if (btn === topRow[0]) {
-      setCurrentDisplay(0);
+      setCurrentDisplay("0");
       setDisplayArr([0])
+      setPreviousNumber("0")
+      setScreenRefresh(false)
     }
 
-    if (btn === topRow[1]) {
-      if (currentDisplay !== 0) {
-        setCurrentDisplay(currentDisplay * -1)
-      }
-    }
+    //make negative or positive
+    // if (btn === topRow[1]) {
+    //   if (currentDisplay !== 0) {
+    //     setCurrentDisplay(currentDisplay * -1)
+    //   }
+    // }
 
   }
   return (
